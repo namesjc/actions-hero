d531e329b85dd9a810958765b2865d129579be44
namesjc <adiachan@outlook.com>
2024-05-22 23:48:59 +0800

    add external dns installation

---
+++ scripts/kind-add-external-dns.sh
@@ -0,0 +1,19 @@
+#!/bin/env bash
+export PATH=$PATH:/usr/local/bin
+
+
+kubectl get ns external-dns 1>/dev/null || kubectl create ns external-dns
+helm repo add external-dns https://kubernetes-sigs.github.io/external-dns/
+helm repo update
+
+cat > external-dns-values.yaml <<EOF
+provider: rdns
+env:
+  - name: ETCD_URLS
+    value: "http://192.168.2.195:2379"
+  - name: RDNS_ROOT_DOMAIN
+    value: lb.rancher.cloud
+EOF
+
+helm upgrade --install external-dns -n external-dns external-dns/external-dns --values external-dns-values.yaml
+kubectl wait pods -n external-dns -l app.kubernetes.io/instance=external-dns--for=condition=ready pod --timeout=90s
