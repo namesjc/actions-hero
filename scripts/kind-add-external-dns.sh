7eb8841d44a9ac3f999c5828ad986f2f06cc0d12
Jun CHEN <adiachan.outlook.com>
2024-05-30 06:18:52 +0000

    add verify

--- scripts/kind-add-external-dns.sh
+++ scripts/kind-add-external-dns.sh
@@ -17,4 +17,4 @@ env:
 EOF
 
 helm upgrade --install external-dns -n external-dns external-dns/external-dns --values external-dns-values.yaml
-kubectl wait pods -n external-dns -l app.kubernetes.io/instance=external-dns--for=condition=ready pod --timeout=90s
+kubectl rollout status deployment.apps/external-dns -n external-dns --timeout=3m
